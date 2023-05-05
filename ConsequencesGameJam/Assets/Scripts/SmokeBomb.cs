8f1e8dcccec484d5630be6951505712f74624a70
CrazyTobi <gamermaster56@gmail.com>
2023-05-05 19:05:59 -0400

    Working on Smoke Bomb

---
+++ ConsequencesGameJam/Assets/Scripts/SmokeBomb.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SmokeBomb : MonoBehaviour
+{
+    public ParticleSystem smoke;
+    public Gadgets gadget;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
